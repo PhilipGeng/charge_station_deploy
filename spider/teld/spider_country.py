24ace7e6119ecc68ac01e92ddd74046109f62ff5
wangleye <wangleye@gmail.com>
2017-12-03 22:56:56 +0800

    teld API changed, make the corresponding change

--- spider/teld/spider_country.py
+++ spider/teld/spider_country.py
@@ -8,6 +8,7 @@ import os
 import time
 from random import randint
 import socket
+import json
 
 socket.setdefaulttimeout(2*60) # wait for maximum two miniutes for downloading the file
 PROXY_FILE = './proxies.csv'
@@ -27,7 +28,7 @@ while(True):
 	for province in province_list:
 		# for proxy_url in proxy_list:
 		while(True):
-			try:
+			# try:
 				# print("try downlaod {} ...".format(proxy_url))
 				proxy_url = 'no'
 				print("downloading....")
@@ -43,15 +44,34 @@ while(True):
 				urlrequest.install_opener(opener)
 
 				print(province, "...")
-				directory = './{}'.format(province)
+				day_str = currentTime.strftime("%Y-%m-%d")
+				directory = './{}/{}'.format(province, day_str)
 				if not os.path.exists(directory):
 					os.makedirs(directory)
-				country_all_url = "https://www.teld.cn/StationNetwork/GetStationNetword?ProvinceName={}&rows=2000".format(urllib.parse.quote(province.encode('utf-8')))
+				
+				filter_str = '{"ProvinceName":"北京市","' + province + '":"","RegionName":"","KeyWord":"","Visible":"1","page":1,"rows":10,"Type":"","StaOpState":"3"}'
+				params = urllib.parse.urlencode({'SID':'CSM-GetStationInfoByFilter', 'filter': filter_str})
+				country_all_url = "https://csm.teld.cn/api/invoke?"+params
 				currentTime = datetime.now()
 				time_str = currentTime.strftime("%Y-%m-%d-%H-%M-%S")
-				urlrequest.urlretrieve(country_all_url,"./{}/{}.json".format(province, time_str))
+				station_file = "{}/{}_station.json".format(directory, time_str)
+				urlrequest.urlretrieve(country_all_url, station_file)
+
+				free_pile_url = "http://ps.teld.cn/api/invoke?SID=CM-GetPileFreeCount"
+				with open(station_file, encoding='utf8') as f:
+					station_json = json.load(f, encoding='utf8') # need to load two times to escape extra backslash in json file
+					station_json = json.loads(station_json)
+					all_station_ids = [element['ID'] for element in station_json['staList']]
+					all_station_ids_str = '{"StaIds":['+','.join('"{0}"'.format(w) for w in all_station_ids)+']}'
+					query_station_ids = {'parameter': all_station_ids_str}
+					post_data = urllib.parse.urlencode(query_station_ids).encode('ascii')
+					#print(post_data)
+					free_pile_file = "{}/{}_free_pile.json".format(directory, time_str)
+					urlrequest.urlretrieve(free_pile_url, free_pile_file, data=post_data)
+
+					
 				time.sleep(randint(20,30))
 				break
-			except:
-				print("exception, wait 10 miinutes, and try again...")
-				time.sleep(10*60)
+			# except:
+			# 	print("exception, wait 10 miinutes, and try again...")
+			# 	time.sleep(10*60)
